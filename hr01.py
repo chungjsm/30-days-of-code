7d07ec40d777b2b8a451d4ea2b15aaa99a0fce69
Michael Chung <chungjsm@gmail.com>
2017-04-18 15:53:29 -0500

    initial commit

---
+++ hr01.py
@@ -0,0 +1,15 @@
+import fileinput
+
+# Declare second integer, double, and String variables.
+i1 = 12
+d1 = 4.0
+s1 = "is the best place to learn and practice coding!"
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+print i + i1
+# Print the sum of the double variables on a new line.
+print d + d1
+# Concatenate and print the String variables on a new line
+print s + s1
+# The 's' variable above should be printed first.
---
+++ hr05.py
@@ -0,0 +1,8 @@
+#!/bin/python
+
+import sys
+
+n = int(raw_input().strip())
+
+for i in range(1, 11):
+    print "%d x %d = %d" % (n, i, n*i)
